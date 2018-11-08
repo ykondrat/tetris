fb64aa44a581c612f755b309f58e2a1e82691fc3
Yevhen Kondratyev <ykondrat@e1r2p14.unit.ua>
2018-11-08 17:13:21 +0200

    start lobby

--- source/components/Main/index.js
+++ source/components/Main/index.js
@@ -13,11 +13,54 @@ export default class Main extends Component {
         return (
             <Fragment>
                 <header>
-                    
+                    <h1>Lobby</h1>
                 </header>
-                <section >
+                <div>
+                    <section>
+                        <h4>Only players</h4>
+                        <div>
+                            <div>
+                                <p>Player nickname</p>
+                                <img src="" alt="Online player rank"/>
+                            </div>
+                        </div>
+                    </section>
+                    <aside>
+                        <h4>Tetris chat</h4>
+                        <div>
+                            <div>
+                                <div>
+                                    <p>nickname</p>
+                                    <img src="" alt="Chat user rank image"/>
+                                </div>
+                                <p>Massage text</p>
+                            </div>
+                        </div>
+                        <div>
+                            <input type="text"/>
+                            <button>Send</button>
+                        </div>
+                    </aside>
+                </div>
 
+
+                <button>Start Game</button>
+
+                <section>
+                    <label htmlFor="">
+                        <input type="radio" />
+                        Solo
+                    </label>
+                    <label htmlFor="">
+                        <input type="radio" />
+                        1vs1
+                    </label>
+                    <label htmlFor="">
+                        <input type="radio" />
+                        Multiplayer (max 4 person)
+                    </label>
                 </section>
+
             </Fragment>
         );
     }
